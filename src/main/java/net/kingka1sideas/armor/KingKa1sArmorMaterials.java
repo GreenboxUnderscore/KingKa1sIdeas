ba9f3fb5f5143dc19efa56615bf7fce692bdec0d
Kevin <greenboxunderscore@gmail.com>
2022-12-24 00:15:35 -0500

    fixed shadow ingot not being used as repair item

--- src/main/java/net/kingka1sideas/armor/KingKa1sArmorMaterials.java
+++ src/main/java/net/kingka1sideas/armor/KingKa1sArmorMaterials.java
@@ -1,5 +1,6 @@
 package net.kingka1sideas.armor;
 
+import net.kingka1sideas.registry.KingKa1sItems;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Items;
@@ -11,33 +12,15 @@ import net.minecraft.util.Lazy;
 import java.util.function.Supplier;
 
 public enum KingKa1sArmorMaterials implements ArmorMaterial {
-    LEATHER("leather", 5, new int[]{1, 2, 3, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.LEATHER);
-    }),
-    CHAIN("chainmail", 15, new int[]{1, 4, 5, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.IRON_INGOT);
-    }),
-    IRON("iron", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.IRON_INGOT);
-    }),
-    GOLD("gold", 7, new int[]{1, 3, 5, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.GOLD_INGOT);
-    }),
-    DIAMOND("diamond", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.DIAMOND);
-    }),
-    OBSIDIAN("diamond", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.OBSIDIAN);
-    }),
-    TURTLE("turtle", 25, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_TURTLE, 0.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.SCUTE);
-    }),
-    NETHERITE("netherite", 37, new int[]{3, 6, 8, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, () -> {
-        return Ingredient.ofItems(Items.NETHERITE_INGOT);
-    }),
-    SHADOW("netherite", 37, new int[]{3, 6, 8, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, () -> {
-        return Ingredient.ofItems(Items.NETHERITE_INGOT);
-    });
+    LEATHER("leather", 5, new int[]{1, 2, 3, 1}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F, 0.0F, () -> Ingredient.ofItems(Items.LEATHER)),
+    CHAIN("chainmail", 15, new int[]{1, 4, 5, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0.0F, 0.0F, () -> Ingredient.ofItems(Items.IRON_INGOT)),
+    IRON("iron", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> Ingredient.ofItems(Items.IRON_INGOT)),
+    GOLD("gold", 7, new int[]{1, 3, 5, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F, 0.0F, () -> Ingredient.ofItems(Items.GOLD_INGOT)),
+    DIAMOND("diamond", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, 0.0F, () -> Ingredient.ofItems(Items.DIAMOND)),
+    OBSIDIAN("obsidian", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ENTITY_GENERIC_EXTINGUISH_FIRE, 2.0F, 0.0F, () -> Ingredient.ofItems(Items.OBSIDIAN)),
+    TURTLE("turtle", 25, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_TURTLE, 0.0F, 0.0F, () -> Ingredient.ofItems(Items.SCUTE)),
+    NETHERITE("netherite", 37, new int[]{3, 6, 8, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, () -> Ingredient.ofItems(Items.NETHERITE_INGOT)),
+    SHADOW("shadow", 37, new int[]{3, 6, 8, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 3.0F, 0.1F, () -> Ingredient.ofItems(KingKa1sItems.SHADOW_INGOT));
 
     private static final int[] BASE_DURABILITY = new int[]{13, 15, 16, 11};
     private final String name;
--- src/main/java/net/kingka1sideas/items/tools/KingKa1sToolMaterial.java
+++ src/main/java/net/kingka1sideas/items/tools/KingKa1sToolMaterial.java
@@ -10,42 +10,18 @@ import net.minecraft.util.Lazy;
 import java.util.function.Supplier;
 
 public enum KingKa1sToolMaterial implements ToolMaterial {
-    WOOD(0, 59, 2.0F, 0.0F, 15, () -> {
-        return Ingredient.fromTag(ItemTags.PLANKS);
-    }),
-    FLINT(1, 131, 4.0F, 1.0F, 5, () -> {
-        return Ingredient.ofItems(Items.FLINT);
-    }),
-    STONE(1, 131, 4.0F, 1.0F, 5, () -> {
-        return Ingredient.fromTag(ItemTags.STONE_TOOL_MATERIALS);
-    }),
-    IRON(2, 250, 6.0F, 2.0F, 14, () -> {
-        return Ingredient.ofItems(Items.IRON_INGOT);
-    }),
-    COPPER(2, 250, 6.0F, 2.0F, 14, () -> {
-        return Ingredient.ofItems(Items.COPPER_INGOT);
-    }),
-    DIAMOND(3, 1561, 8.0F, 3.0F, 10, () -> {
-        return Ingredient.ofItems(Items.DIAMOND);
-    }),
-    AMETHYST(3, 1561, 8.0F, 3.0F, 10, () -> {
-        return Ingredient.ofItems(Items.AMETHYST_SHARD);
-    }),
-    EMERALD(3, 1561, 8.0F, 3.0F, 10, () -> {
-        return Ingredient.ofItems(Items.EMERALD);
-    }),
-    GOLD(0, 32, 12.0F, 0.0F, 22, () -> {
-        return Ingredient.ofItems(Items.GOLD_INGOT);
-    }),
-    OBSIDIAN(3, 2031, 12.0F, 4.0F, 30, () -> {
-        return Ingredient.ofItems(Items.OBSIDIAN);
-    }),
-    NETHERITE(4, 2031, 9.0F, 4.0F, 15, () -> {
-        return Ingredient.ofItems(Items.NETHERITE_INGOT);
-    }),
-    SHADOW(4, 4062, 12.0F, 4.0F, 30, () -> {
-        return Ingredient.ofItems(KingKa1sItems.SHADOW_INGOT);
-    });
+    WOOD(0, 59, 2.0F, 0.0F, 15, () -> Ingredient.fromTag(ItemTags.PLANKS)),
+    FLINT(1, 131, 4.0F, 1.0F, 5, () -> Ingredient.ofItems(Items.FLINT)),
+    STONE(1, 131, 4.0F, 1.0F, 5, () -> Ingredient.fromTag(ItemTags.STONE_TOOL_MATERIALS)),
+    IRON(2, 250, 6.0F, 2.0F, 14, () -> Ingredient.ofItems(Items.IRON_INGOT)),
+    COPPER(2, 250, 6.0F, 2.0F, 14, () -> Ingredient.ofItems(Items.COPPER_INGOT)),
+    DIAMOND(3, 1561, 8.0F, 3.0F, 10, () -> Ingredient.ofItems(Items.DIAMOND)),
+    AMETHYST(3, 1561, 8.0F, 3.0F, 10, () -> Ingredient.ofItems(Items.AMETHYST_SHARD)),
+    EMERALD(3, 1561, 8.0F, 3.0F, 10, () -> Ingredient.ofItems(Items.EMERALD)),
+    GOLD(0, 32, 12.0F, 0.0F, 22, () -> Ingredient.ofItems(Items.GOLD_INGOT)),
+    OBSIDIAN(3, 2031, 12.0F, 4.0F, 30, () -> Ingredient.ofItems(Items.OBSIDIAN)),
+    NETHERITE(4, 2031, 9.0F, 4.0F, 15, () -> Ingredient.ofItems(Items.NETHERITE_INGOT)),
+    SHADOW(4, 4062, 12.0F, 4.0F, 30, () -> Ingredient.ofItems(KingKa1sItems.SHADOW_INGOT));
 
     private final int miningLevel;
     private final int itemDurability;
